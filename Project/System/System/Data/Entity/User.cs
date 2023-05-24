9c623fd8e343fe554cd45081bf024064c0c0c69f
AlexC <alex123321choicy@gmail.com>
2023-05-24 15:59:56 +0800

    alex sync

---
+++ Project/System/System/Data/Entity/User.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Data.Entity
+{
+    public class User
+    {
+        [Required]
+        public String StaffID { get; set; }
+        [Required]
+        public String Position { get; set; }
+        [Required]
+        public String DeptID { get; set; }
+        [Required]
+        public String ResturantID { get; set; }
+        [Required]
+        public String FirstName { get; set; }
+        [Required]
+        public String LastName { get; set; }
+        [Column(TypeName = "char(1)")]
+        public sexEnum sex { get; set; }
+        [Required]
+        public String Phone { get; set; }
+        [Required]
+        public String birthdate { get; set; }
+        [Required]
+        public String password {get;set;}
+        public String remark { get; set; }
+
+    }
+
+    public enum sexEnum
+    {
+        M,F
+    }
+}
