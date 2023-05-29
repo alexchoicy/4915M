b80fb17adad65768cde3fca6b24856aedc3fb36c
AlexC <alex123321choicy@gmail.com>
2023-05-29 20:59:21 +0800

    add item back

---
+++ Project/System/System/Controllers/Input/CategoryModel.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Server.Model.Entity;
+
+namespace Server.Controllers.Input
+{
+    public class MapCate : Profile
+    {
+        public MapCate() {
+            CreateMap<CategoryAccessControlModel, AccessControl>();
+        
+        }
+    }
+    public class CategoryItemModel
+    {
+        public string ItemID { get; set; }
+        public string CategoryID { get; set; }
+
+    }
+    public class CategoryAccessControlModel
+    {
+        public string CategoryID { get; set;}
+        public string TypeID { get; set;}
+    }
+}
